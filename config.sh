940df97a438fc862b2f2a5568e1fc0d4be4ae4c0
Eric Sandall <sandalle@gmail.com>
2017-11-08 16:09:47 -0800

    Forge Mod Loader 1.12.2-14.23.0.2532

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #just to set some globals used by the other scripts
-FORGE='forge-1.12.2-14.23.0.2528-universal.jar'
+FORGE='forge-1.12.2-14.23.0.2532-universal.jar'
 SERVER_DIR="$(pwd)"
 START_MEMORY=4096
 MAX_MEMORY=4096
