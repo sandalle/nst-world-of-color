4ba79d659f5b6425562f72054483854d2bf01a19
Eric Sandall <sandalle@gmail.com>
2018-02-10 10:02:24 -0800

    Make release.sh work on OS X or Linux

--- release.sh
+++ release.sh
@@ -4,13 +4,17 @@
 GPG_USER='sandalle@gmail.com'
 VERSION=$(cat VERSION)
 NEXT_VERSION=$(perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e' <<< ${VERSION})
+OS=$(uname)
 
 # Tag current version
 git tag -u ${GPG_USER} --sign -m "nst-world-of-color-${VERSION}" "nst-world-of-color-${VERSION}"  &&
 
 # Increment to next version
 echo ${NEXT_VERSION} > VERSION  &&
-sed -i '' "s/S:modPackVersion=.*/S:modPackVersion=${NEXT_VERSION}/" config/Mercurius.cfg  &&
+case ${OS} in
+  "Linux")  sed -i    "s/S:modPackVersion=.*/S:modPackVersion=${NEXT_VERSION}/" config/Mercurius.cfg ;;
+  "Darwin") sed -i '' "s/S:modPackVersion=.*/S:modPackVersion=${NEXT_VERSION}/" config/Mercurius.cfg ;;
+esac &&
 git commit -m "Starting ${NEXT_VERSION}" VERSION config/Mercurius.cfg  &&
 
 # Push new version and tags
