bc1649ff572a47ad346ac71d983093fd514adee4
Eric Sandall <sandalle@gmail.com>
2018-09-08 13:48:31 -0700

    Updated mods and regenerated Project E EMC
    Forge Mod Loader 1.12.2-14.23.4.2760
    ActuallyAdditions-1.12.2-r140
    AppleCore-mc1.12.2-3.1.4
    AutoRegLib-1.3-20
    Botania r1.10-356
    BrandonsCore-1.12.2-2.4.4.173-universal
    Chisel-MC1.12.2-0.2.1.34
    chiselsandbits-14.23
    CodeChickenLib-1.12.2-3.2.1.351-universal
    CoFHCore-1.12.2-4.5.3.20-universal
    colytra-1.12.2-1.1.0
    CraftTweaker2-1.12-4.1.9
    CTM-MC1.12.2-0.3.2.18
    Cucumber-1.12.2-1.1.1
    Draconic-Evolution-1.12.2-2.3.13.306-universal
    DynamicSurroundings-1.12.2-3.4.9.15
    EnderStorage-1.12.2-2.4.5.135-universal
    foamfix-0.10.1-1.12.2
    forestry_1.12.2-5.8.1.322
    Forgelin-1.7.4
    FTBLib-5.3.0.48
    FTBUtilities-5.3.0.48
    funky-locomotion-1.12.2-1.1.2
    furniture-5.9.1-1.12.2
    GalacticraftCore-1.12.2-4.0.1.181
    Galacticraft-Planets-1.12.2-4.0.1.181
    Guide-API-1.12-2.1.6-61
    ImmersiveEngineering-0.12-85
    JEHC-1.12.2-1.6.1.0
    jei_1.12.2-4.11.0.212
    librarianlib-1.12.2-4.15
    lostcities-1.12-2.0.10
    mcjtylib-1.12-3.0.4
    metamorph-1.1.6-1.12.2
    MicdoodleCore-1.12.2-4.0.1.181
    morebees-1.12.2-2.0.1
    MysticalAgriculture-1.12-1.6.13
    natura-1.12.2-4.3.2.62
    NetherPortalFix_1.12.1-5.3.17
    NoMoreRecipeConflict-0.11(1.12.2)
    NuclearCraft-2.11c--1.12.2
    omlib-1.12.2-3.0.1-162
    ompd-1.12.2-3.0.0-36
    openmodularturrets-1.12.2-3.0.1-264
    OreCore 1.12.2 (v.1.3.9)
    Pam's HarvestCraft 1.12.2y
    QuantumStorage-1.12-4.6.2
    Quark-r1.5-129
    RebornCore-1.12.2-3.10.0.332-universal
    RebornStorage-1.12.2-3.2.2.65
    refinedstorage-1.6.4
    refinedstorageaddons-0.4.2
    rftools-1.12-7.55
    rftoolsctrl-1.12-1.9.1
    StevesCarts-1.12.2-2.4.23.113
    StorageDrawers-1.12.2-5.3.7
    TConstruct-1.12.2-2.10.1.87
    Thaumcraft-1.12.2-6.1.BETA24
    TickProfiler-1.12-0.0.6
    topaddons-1.12.2-1.9.0
    twilightforest-1.12.2-3.8.654-universal
    UniDict-1.12.2-2.7b
    update-list.sh
    usefulnullifiers-1.5.0
    wizardry-0.9.7
    xnet-1.12-1.7.3

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #just to set some globals used by the other scripts
-FORGE='forge-1.12.2-14.23.4.2705-universal.jar'
+FORGE='forge-1.12.2-14.23.4.2760-universal.jar'
 SERVER_DIR="$(pwd)"
 START_MEMORY=4096
 MAX_MEMORY=4096
