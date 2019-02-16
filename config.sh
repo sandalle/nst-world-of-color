760d6ec7f21f27dd01891317d0b62a01c2155448
Eric Sandall <sandalle@gmail.com>
2019-02-16 13:41:22 -0800

    Update Forge Mod Loader to 1.12.2-14.23.5.2814
    Set minimum memory to 2G, leave maximum at 4G.

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #just to set some globals used by the other scripts
-FORGE='forge-1.12.2-14.23.5.2808-universal.jar'
+FORGE='forge-1.12.2-14.23.5.2814-universal.jar'
 SERVER_DIR="$(pwd)"
-START_MEMORY=4096
+START_MEMORY=2048
 MAX_MEMORY=4096
 
 [[ -f /etc/profile.d/java.sh ]] && . /etc/profile.d/java.sh
