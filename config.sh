cd8d4881cce517fd761b6ab2890180b0fc2f28a2
Eric Sandall <sandalle@gmail.com>
2019-04-20 11:26:19 -0700

    Updated mods:
    Forge Mod Loader 1.12.2-14.23.5.2836
    astralsorcery-1.12.2-1.10.17
    Botania r1.10-361
    BrandonsCore-1.12.2-2.4.10.198-universal
    CraftTweaker2-1.12-4.1.17
    Draconic-Evolution-1.12.2-2.3.21.342-universal
    EmbersRekindled-1.13-hotfix2
    ExtraPlanets-1.12.2-0.5.2
    foamfix-0.10.5-1.12.2
    FTBLib-5.4.1.96
    FTBUtilities-5.4.0.94
    FTBUtilitiesBackups-1.0.0.3
    GalacticraftCore-1.12.2-4.0.2.210
    Galacticraft-Planets-1.12.2-4.0.2.210
    HammerCore-1.12.2-2.0.4.4
    jehc-1.12.2-1.6.3
    JustEnoughResources-1.12.2-0.9.0.53
    Mantle-1.12-1.3.3.49
    MicdoodleCore-1.12.2-4.0.2.210
    NotEnoughIDs-1.5.4.4
    NuclearCraft-2.15-1.12.2
    OpenBlocks-1.12.2-1.8.1
    OpenModsLib-1.12.2-0.12.2
    Pam's HarvestCraft 1.12.2zc The 7mm
    Psi-r1.1-74
    RebornCore-1.12.2-3.13.12.447-universal
    StevesCarts-1.12.2-2.4.30.134
    TConstruct-1.12.2-2.12.0.135
    ThaumicAdditions-1.12.2-13r
    ThaumicJEI-1.12.2-1.5.8-26
    topaddons-1.12.2-1.11.0
    
    Regenerate Project E EMC.

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #just to set some globals used by the other scripts
-FORGE='forge-1.12.2-14.23.5.2823-universal.jar'
+FORGE='forge-1.12.2-14.23.5.2836-universal.jar'
 SERVER_DIR="$(pwd)"
 START_MEMORY=2048
 MAX_MEMORY=4096
