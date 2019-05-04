8db8cd8e9dc7d9322c9b9035e4e2bfa8a52f0315
Eric Sandall <sandalle@gmail.com>
2019-05-04 01:05:01 -0700

    Increase default memory for server for performance
    Minimum 4g, maximum 6g.

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #just to set some globals used by the other scripts
 FORGE='forge-1.12.2-14.23.5.2836-universal.jar'
 SERVER_DIR="$(pwd)"
-START_MEMORY=2048
-MAX_MEMORY=4096
+START_MEMORY=4096
+MAX_MEMORY=6144
 
 [[ -f /etc/profile.d/java.sh ]] && . /etc/profile.d/java.sh
